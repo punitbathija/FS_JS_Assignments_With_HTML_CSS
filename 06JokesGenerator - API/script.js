f7fdf459086879344662049d2a6fe41758d2fa54
Punit Bathija <87932246+punitbathija@users.noreply.github.com>
2022-09-26 18:32:28 +0530

    Update script.js
    Assgigment Completed Get Dad Jokes API

---
+++ 06JokesGenerator - API/script.js
@@ -0,0 +1,27 @@
+// Targeting all the elemnets needed to execute the task
+
+let jokeBody = document.querySelector("#joke");
+let jokeBtn = document.querySelector("#jokeBtn");
+
+// Function to get the joke from the api
+
+async function getJoke(){
+    const res = await fetch("http://icanhazdadjoke.com", {
+        headers: {
+            Accept : "application/json",
+        },
+    });
+
+    return res.json();
+}
+
+// Function to refresh the joke
+
+async function handleClick(){
+    const { joke } = await getJoke();
+    jokeBody.textContent = joke;
+}
+
+// Adding event listener to the button
+
+jokeBtn.addEventListener("click", handleClick);
